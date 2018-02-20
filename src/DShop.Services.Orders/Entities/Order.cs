e360c378bd96512bbf912a2476e5fc40d4caa12b
GooRiOn <foreverframenet@gmail.com>
2018-02-20 20:29:19 +0100

    Added Order entity

---
+++ src/DShop.Services.Orders/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using DShop.Messages.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DShop.Services.Orders.Entities
+{
+    public class Order : BaseEntity
+    {
+        public Guid CustomerId { get; protected set; }
+        public long Number { get; protected set; }
+        public IEnumerable<Guid> ProductIds { get; protected set; }
+        public decimal TotalAmount { get; protected set; }
+    }
+}
