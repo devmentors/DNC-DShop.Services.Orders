e5f11577c52ff8c09301da57b21d46d5c737c94b
GooRiOn <foreverframenet@gmail.com>
2018-02-24 19:34:34 +0100

    Added Order entity

--- src/DShop.Services.Orders/Entities/Order.cs
+++ src/DShop.Services.Orders/Entities/Order.cs
@@ -1,8 +1,7 @@
-﻿using DShop.Messages.Entities;
+﻿using DShop.Common.Types;
+using DShop.Messages.Entities;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DShop.Services.Orders.Entities
 {
@@ -12,5 +11,43 @@ namespace DShop.Services.Orders.Entities
         public long Number { get; protected set; }
         public IEnumerable<Guid> ProductIds { get; protected set; }
         public decimal TotalAmount { get; protected set; }
+        public OrderStatus Status { get; protected set; }
+
+        public Order(Guid id, Guid customerId, long number, IEnumerable<Guid> productIds, decimal totalAmount)
+            :base(id)
+        {
+            CustomerId = customerId;
+            Number = number;
+            ProductIds = productIds;
+            TotalAmount = TotalAmount;
+            Status = OrderStatus.Created;
+        }
+
+        public void CompleteOrder()
+        {
+            if(Status == OrderStatus.Canceled)
+            {
+                throw new DShopException("Cannot complete canceled order.");
+            }
+
+            Status = OrderStatus.Completed;
+        }
+
+        public void CancelOrder()
+        {
+            if (Status == OrderStatus.Completed)
+            {
+                throw new DShopException("Cannot cancel completed order.");
+            }
+
+            Status = OrderStatus.Canceled;
+        }
+
+        public enum OrderStatus : byte
+        {
+            Created = 0,
+            Completed = 1,
+            Canceled = 2,
+        }
     }
 }
--- src/DShop.Services.Orders/Program.cs
+++ src/DShop.Services.Orders/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using static DShop.Services.Orders.Entities.Order;
 
 namespace DShop.Services.Orders
 {
