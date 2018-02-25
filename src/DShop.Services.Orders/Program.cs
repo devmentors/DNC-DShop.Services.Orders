194f1bbb5d75e479e7005925282f5bdc11090566
GooRiOn <foreverframenet@gmail.com>
2018-02-25 14:30:37 +0100

    Clean up

--- src/DShop.Services.Orders/Program.cs
+++ src/DShop.Services.Orders/Program.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using static DShop.Services.Orders.Entities.Order;
 
 namespace DShop.Services.Orders
 {
--- src/DShop.Services.Orders/Startup.cs
+++ src/DShop.Services.Orders/Startup.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using DShop.Common.Mongo;
@@ -12,8 +9,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace DShop.Services.Orders
 {
