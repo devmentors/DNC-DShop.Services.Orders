afbcc2b348f373112b0e36edea9f66f22a652ba7
Piotr Gankiewicz <spetz90@gmail.com>
2018-03-15 09:07:35 +0100

    Travis, Docker

---
+++ scripts/publish.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+dotnet publish --no-restore ./src/DShop.Services.Orders -c Release -o ./bin/Docker
+
+DOCKER_ENV=''
+DOCKER_TAG=''
+
+case "$TRAVIS_BRANCH" in
+  "master")
+    DOCKER_ENV=production
+    DOCKER_TAG=latest
+    ;;
+  "develop")
+    DOCKER_ENV=development
+    DOCKER_TAG=dev
+    ;;    
+esac
+
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker build -f ./src/DShop.Services.Orders/Dockerfile.$DOCKER_ENV -t dshop.services.orders:$DOCKER_TAG ./src/DShop.Services.Orders
+docker tag dshop.services.orders:$DOCKER_TAG $DOCKER_USERNAME/dshop.services.orders:$DOCKER_TAG
+docker push $DOCKER_USERNAME/dshop.services.orders:$DOCKER_TAG
