15984b6ca0f7cf478027b5522a349c09b1be7a71
Piotr Gankiewicz <spetz90@gmail.com>
2018-06-25 10:05:25 +0200

    .NET Core 2.1.1 update

--- src/DShop.Services.Orders/Program.cs
+++ src/DShop.Services.Orders/Program.cs
@@ -8,13 +8,12 @@ namespace DShop.Services.Orders
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseLockbox()
-                .Build();
+                .UseLockbox();
     }
 }
