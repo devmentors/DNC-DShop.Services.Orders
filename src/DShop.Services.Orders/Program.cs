1a36b5fd46f3a577bab752c50ed950d3da747780
Piotr Gankiewicz <spetz90@gmail.com>
2018-08-26 12:40:29 +0200

    Vault integration with Configuration on Startup

--- src/DShop.Services.Orders/Program.cs
+++ src/DShop.Services.Orders/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using DShop.Common.Metrics;
 using DShop.Common.Mvc;
+using DShop.Common.Vault;
 
 namespace DShop.Services.Orders
 {
@@ -15,6 +16,7 @@ namespace DShop.Services.Orders
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseVault()
                 .UseLockbox()
                 .UseAppMetrics();
     }
