1d9e6bb5b4534d3efda30dea19ad5efa59480e6a
GooRiOn <foreverframenet@gmail.com>
2018-09-24 22:47:34 +0200

    Added handler for CreateOrderDiscount command

---
+++ src/DShop.Services.Orders/Handlers/Orders/CreateOrderDiscountHandler.cs
@@ -0,0 +1,17 @@
+﻿using DShop.Common.Handlers;
+using DShop.Common.RabbitMq;
+using DShop.Services.Orders.Messages.Commands;
+using System;
+using System.Threading.Tasks;
+
+namespace DShop.Services.Orders.Handlers.Orders
+{
+    public sealed class CreateOrderDiscountHandler : ICommandHandler<CreateOrderDiscount>
+    {
+        public Task HandleAsync(CreateOrderDiscount command, ICorrelationContext context)
+        {
+            Console.WriteLine("I was triggered by SAGA!!!");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/DShop.Services.Orders/Messages/Commands/CreateOrderDiscount.cs
@@ -0,0 +1,21 @@
+﻿using DShop.Common.Messages;
+using Newtonsoft.Json;
+using System;
+
+namespace DShop.Services.Orders.Messages.Commands
+{
+    public class CreateOrderDiscount : ICommand
+    {
+        public Guid Id { get; }
+        public Guid CustomerId { get; }
+        public long Number { get; }
+
+        [JsonConstructor]
+        public CreateOrderDiscount(Guid id, Guid customerId, long number)
+        {
+            Id = id;
+            CustomerId = customerId;
+            Number = number;
+        }
+    }
+}
